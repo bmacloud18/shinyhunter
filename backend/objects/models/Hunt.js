f08a6293527c5966e94208f5164dd38f29ea4c97
bmabbott <bmabbott@ncsu.edu>
2023-12-13 00:07:17 -0500

    added hunt model, finalizing db setup

--- backend/objects/models/Hunt.js
+++ backend/objects/models/Hunt.js
@@ -1,5 +1,59 @@
 module.exports = class {
     id = null;
-    name = null;
-    
-}
+    pkm = null;
+    user = null;
+    start_date_string = null;
+    start_date_display= null;
+    end_date_string = null;
+    end_date_display = null;
+    hunt_time = null;
+
+    constructor (data) {
+        this.id = data.hnt_id;
+        this.pkm = data.pkm_id;
+        this.user = data.usr_id;
+        this.start_date_string = data.hnt_start_date_string;
+        this.start_date_display = convertDate(data.hnt_start_date_string);
+        this.end_date_string = data.hnt_end_date_string;
+        this.end_date_display = convertDate(data.hnt_end_date_string);
+        this.hunt_time = data.hnt_time_ms;
+    }
+};
+
+function convertDate(date) {
+    let eventdate = new Date(date)
+    let datestring = eventdate.toDateString();
+    let timestring = eventdate.toLocaleTimeString();
+  
+    let dws = 0;
+    let idx = 0;
+    let fullstring = "";
+    let dcc = datestring.charAt(idx);
+    while (dws < 3)
+    {
+        if (dcc == " ") {
+            dws++;
+        }
+        fullstring = fullstring + dcc;
+        idx++;
+        dcc = datestring.charAt(idx);
+    }
+  
+    fullstring = fullstring.substring(0, fullstring.length - 1) + ", ";
+  
+    idx = 0;
+    let tcol = 0;
+    let tcc = timestring.charAt(idx);
+    while (tcol < 2)
+    {
+        if (tcc == ":") {
+            tcol++;
+        }
+        fullstring = fullstring + tcc;
+        idx++;
+        tcc = timestring.charAt(idx);
+    }
+  
+    let timesuffix = timestring.substring(timestring.length - 2, timestring.length) == "PM" ? "pm" : "am";
+    return fullstring.substring(0, fullstring.length - 1) + timesuffix;
+};
