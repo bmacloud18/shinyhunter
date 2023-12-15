d7ae6235febd15d57c86f699325ccfa26566f9e9
bmabbott <bmabbott@ncsu.edu>
2023-12-15 17:16:03 -0500

    added all currently planned pages, updated routes, updated app.js for hbs, added home and hunt js files for titles

--- frontend/src/frontendRoutes.js
+++ frontend/src/frontendRoutes.js
@@ -4,30 +4,73 @@ const router = express.Router();
 router.use(express.static('static'));
 router.use(express.urlencoded({extended: true}));
 
-const path = require('path');
-const html_dir = path.join(__dirname ,'../../templates/');
-
-//base
-router.get('/', (req, res) => {
-    res.sendFile(`${html_dir}index.html`);
+// base
+router.get( '/', ( req, res ) => {
+    res.redirect( '/login' );
 });
 
-//index
-router.get('/index', (req, res) => {
-    res.sendFile(`${html_dir}index.html`);
+// login
+router.get( '/login', ( req, res ) => {
+    const data = { title: 'Welcome!' };
+    res.render('login', data );
 });
 
-//profile details
-router.get('/userprofile', (req, res) => {
-    res.sendFile(`${html_dir}userprofile.html`);
+// login
+router.get( '/login', ( req, res ) => {
+    const data = { title: 'Welcome!' };
+    res.render('login', data );
 });
 
-//sign in
-router.get('/login', (req, res) => {
-    res.sendFile(`${html_dir}login.html`);
+// home
+router.get( '/home', ( req, res ) => {
+    const data = { title: 'Welcome!' };
+    res.render('homepage', data );
 });
 
-//sign up
-router.get('/signup', (req, res) => {
-    res.sendFile(`${html_dir}signup.html`);
+// hunt
+router.get( '/hunt', ( req, res ) => {
+    const data = { title: 'Hunting' };
+    res.render('hunt', data );
+});
+
+// account settings
+router.get( '/accountsettings', ( req, res ) => {
+    const data = { title: 'Settings' };
+    res.render('accountsettings', data );
+});
+
+// add a new finished hunt
+router.get( '/addcaught', ( req, res ) => {
+    const data = { title: 'Add New Hunt' };
+    res.render('addcaught', data );
+});
+
+// caught
+router.get( '/caught', ( req, res ) => {
+    const data = { title: 'Completed Hunts' };
+    res.render('caught', data );
+});
+
+// huntsettings
+router.get( '/huntsettings', ( req, res ) => {
+    const data = { title: 'Hunt Settings' };
+    res.render('huntsettings', data );
+});
+
+// new hunt
+router.get( '/newhunt', ( req, res ) => {
+    const data = { title: 'Begin Hunt' };
+    res.render('newhunt', data );
+});
+
+// successful hunt
+router.get( '/success', ( req, res ) => {
+    const data = { title: 'Congratulations!' };
+    res.render('success', data );
+});
+
+// user profile
+router.get( '/userprofile', ( req, res ) => {
+    const data = { title: 'User Profile' };
+    res.render('userprofile', data );
 });
